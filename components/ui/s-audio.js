008e14cc03ccd35a3cef15e6d41d2a25e1d4b05f
puckerino <loregalafate+github@gmail.com>
2026-07-28 07:36:46 +0200

    Update s-audio.js

--- components/ui/s-audio.js
+++ components/ui/s-audio.js
@@ -102,7 +102,7 @@ export default class SAudio extends HTMLElement {
       <article class="s-audio">
 
         <button class="s-audio-button" type="button" aria-label="Reproducir audio">
-          <i data-lucide="play"></i>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play-icon lucide-play"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"/></svg>
         </button>
 
         <div class="s-audio-content">
@@ -165,7 +165,7 @@ export default class SAudio extends HTMLElement {
       this.playing = true;
 
       this.button.innerHTML =
-        '<i data-lucide="pause"></i>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause-icon lucide-pause"><rect x="14" y="3" width="5" height="18" rx="1"/><rect x="5" y="3" width="5" height="18" rx="1"/></svg>';
 
       this.button.setAttribute(
         "aria-label",
@@ -187,7 +187,7 @@ export default class SAudio extends HTMLElement {
       this.playing = false;
 
       this.button.innerHTML =
-        '<i data-lucide="play"></i>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play-icon lucide-play"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"/></svg>';
 
       this.button.setAttribute(
         "aria-label",
