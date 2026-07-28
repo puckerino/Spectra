0302a2a2b55179f05e5e7e743e461bc8fdc5bfc6
puckerino <loregalafate+github@gmail.com>
2026-07-28 07:37:55 +0200

    Update s-spoiler.js

--- components/ui/s-spoiler.js
+++ components/ui/s-spoiler.js
@@ -70,7 +70,7 @@ export default class S_Spoiler extends HTMLElement {
       <button class="spoiler-toggle">
 
         <span class="spoiler-icon">
-          <i data-lucide="chevron-right"></i>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
         </span>
 
         <span class="spoiler-title">
