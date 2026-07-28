788fe9fec821808255e03a49a6ea20ae16699c37
puckerino <loregalafate+github@gmail.com>
2026-07-28 16:05:39 +0200

    Create s-search.js

---
+++ components/posts/s-search.js
@@ -0,0 +1,63 @@
+export default class SSearch extends HTMLElement {
+  connectedCallback() {
+    if (this.dataset.ready === "true") return;
+    this.dataset.ready = "true";
+
+    const titulo = this.getAttribute("titulo") || "";
+    const subtitulo = this.getAttribute("subtitulo") || "";
+    const imagen = this.getAttribute("imagen") || "";
+    const estado = this.getAttribute("estado") || "";
+    const contenido = this.innerHTML;
+
+    this.innerHTML = `
+      <style>
+        .s-search {
+          display: grid;
+          grid-template-columns: .5fr 1fr;
+        }
+
+        .s-search .s-sidebar {
+          display: flex;
+          flex-direction: column;
+          gap: var(--spacing);
+        }
+
+        .s-search .s-search-title {
+          font: bold var(--f-l) var(--f-title-deco);
+        }
+
+        .s-search .s-search-subtitle {
+          font: var(--f-s) var(--f-metadata);
+          color: var(--text-subtle);
+        }
+
+        .s-search .s-search-media img {
+          display: block;
+          width: 100%;
+          aspect-ratio: 1;
+          object-fit: cover;
+        }
+
+        .s-search .s-search-body {
+          padding: 0 var(--spacing-l);
+          border-left: 1px solid var(--border);
+        }
+      </style>
+
+      <div class="s-search">
+        <div class="s-search-sidebar">
+            <h3 class="s-search-title">${titulo}</h3>
+            <span class="s-search-subtitle">${subtitulo}</span>
+            <figure class="s-search-media">
+              <img src="${imagen}" />
+            </figure>
+        </div>
+
+        <div class="s-search-body">
+          <span class="s-search-estado">${estado}</span>
+          ${contenido}
+        </div>
+      </div>
+    `;
+  }
+}
