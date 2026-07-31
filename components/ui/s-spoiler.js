f92cc39cd15789413c6c8e8a39de1cbd132a68b8
puckerino <loregalafate+github@gmail.com>
2026-07-31 11:22:37 +0200

    Update s-spoiler.js

--- components/ui/s-spoiler.js
+++ components/ui/s-spoiler.js
@@ -40,7 +40,7 @@ export default class S_Spoiler extends HTMLElement {
         }
 
         s-spoiler[open] .spoiler-icon {
-          rotate: 90deg;
+          rotate: 45deg;
         }
 
         s-spoiler .spoiler-content {
@@ -71,7 +71,7 @@ export default class S_Spoiler extends HTMLElement {
       <button class="spoiler-toggle">
 
         <span class="spoiler-icon">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
         </span>
 
         <span class="spoiler-title">
